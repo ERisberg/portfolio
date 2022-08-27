20a1409007f3e87238e3b06181091345660e8cd3
xemris <emil-sj-@hotmail.com>
2022-08-28 00:24:56 +0200

    project section & custom font

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,95 @@
-import React from "react";
+import React, { useRef } from "react";
 import Sidebar from "./components/sidebar";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLink } from "@fortawesome/free-solid-svg-icons";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
 
 function App() {
+  const scrollRefs = useRef<Array<HTMLDivElement | null>>([]);
+
+  const executeScroll = (index: number) =>
+    scrollRefs.current[index]?.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+
   return (
-    <div className="w-screen min-h-screen flex flex-col overflow-hidden">
-      <Sidebar />
-      <section className="w-full h-screen flex justify-center items-center">
-        <h1 className="text-3xl font-bold">Landing page</h1>
-      </section>
+    <div className="w-full min-h-screen flex flex-col overflow-hidden">
+      <Sidebar scrollToProject={(index: number) => executeScroll(index)} />
+      <div className="w-full pl-[17rem] min-h-screen flex flex-col">
+        <section
+          ref={(el: HTMLDivElement) => (scrollRefs.current[0] = el)}
+          className="w-full h-screen flex items-center bg-[#01E08F] p-[3rem]"
+        >
+          <div className="block w-full">
+            <h1 className="text-[6rem] font-[SairaExtraCondensed] leading-[1] mb-0 font-bold text-[#EDF4E0] uppercase select-none">
+              Emil<span className="text-[#05396B]">Risberg</span>
+            </h1>
+            <div className="mb-[3rem] ">
+              <a href="mailto:name@email.com">
+                <p className="text-[#05396B] font-bold uppercase hover:underline text-[1.5rem]">
+                  emilrisberg17@gmail.com
+                </p>
+              </a>
+            </div>
+            <p className="text-[#EDF4E0] text-[1.15rem] font-bold mb-[3rem]">C# utvecklare</p>
+            <button>
+              <FontAwesomeIcon
+                icon={faGithub}
+                size="2x"
+                className="hover:text-[#edf4e0] text-[#05396B] transition-colors ease-in-out duration-100"
+              />
+            </button>
+          </div>
+        </section>
+        <hr className="divide-[#EDF4E0] border-2" />
+        <section
+          ref={(el: HTMLDivElement) => (scrollRefs.current[1] = el)}
+          className="w-full min-h-screen bg-[#389583] flex p-[3rem]"
+        >
+          <div className="block w-full">
+            <h2 className="title">Projekt</h2>
+            <ProjectItem />
+            <ProjectItem />
+            <ProjectItem />
+            <ProjectItem />
+            <ProjectItem />
+          </div>
+        </section>
+        <hr className="divide-[#EDF4E0] border-2" />
+        <section className="w-full bg-[#01E08F] flex p-[3rem]">
+          <h3 className="subheading text-[#05396B]">Footer</h3>
+        </section>
+      </div>
     </div>
   );
 }
+
+const ProjectItem = () => {
+  return (
+    <div className="mb-[3rem]">
+      <div>
+        <h3 className="projectTitle">Title</h3>
+        <div className="subheading mb-[1rem]">Description</div>
+        <p className="mb-[1rem] text-[#05396B]">
+          Startup that allows users to find local runners, start running clubs, and/or schedule running events. Users
+          are incentivized to invite and run with others via real-time chat, invitations, and following/like features.
+        </p>
+        <ul className="text-[#edf4e0] flex space-x-4 transition-colors duration-100 ease-in-out">
+          <li>
+            <button>
+              <FontAwesomeIcon icon={faLink} className="hover:text-[#05396B]" />
+            </button>
+          </li>
+          <li>
+            <button>
+              <FontAwesomeIcon icon={faGithub} className="hover:text-[#05396B]" />
+            </button>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
 export default App;
--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,9 +1,47 @@
-const Sidebar = () => {
+interface SidebarProps {
+  scrollToProject?: (index: number) => void;
+}
+
+const Sidebar = ({ scrollToProject }: SidebarProps) => {
   return (
-    <div className="fixed left-0 top-0 h-screen bg-slate-500">
-      <h1 className="text-3xl font-bold">Sidebar</h1>
-    </div>
+    <nav className="fixed w-[17rem] left-0 top-0 h-screen flex flex-col justify-center items-center bg-[#05396B] z-50">
+      <div>
+        <img
+          src="https://pbs.twimg.com/profile_images/1416909276233281537/WTe5KGlq_400x400.jpg"
+          alt=""
+          className="max-w-[10rem] max-h-[10rem] rounded-full border-[0.5rem] mb-[0.5rem] border-[#8DE4AF]"
+        />
+      </div>
+      <div className="w-full flex justify-center items-center py-[1rem]">
+        <ul className="flex flex-col items-center justify-center space-y-4">
+          <li>
+            <CustomLink label="Hem" index={0} onClick={(index: number) => scrollToProject?.(index)} />
+          </li>
+          <li>
+            <CustomLink label="Projekt" index={1} onClick={(index: number) => scrollToProject?.(index)} />
+          </li>
+        </ul>
+      </div>
+    </nav>
   );
 };
+
+//#region CustomLink
+interface LinkProps {
+  label: string;
+  index: number;
+  onClick?: (index: number) => void;
+}
+
+const CustomLink = ({ label, index, onClick }: LinkProps) => {
+  return (
+    <button onClick={() => onClick?.(index)}>
+      <p className="font-bold tracking-[0.05rem] text-[#EDF4E0] text-lg hover:text-[#5CDB94] transition-colors ease-in-out duration-75">
+        {label}
+      </p>
+    </button>
+  );
+};
+//#endregion
 
 export default Sidebar;
